4c05776cb0e16364afe6d2b385b64f2f5cc02b39
Taka Oyama <toyama4649@hotmail.com>
2023-04-16 23:15:59 +0900

    fix test

--- tests/src/EnumerableTest.php
+++ tests/src/EnumerableTest.php
@@ -8,15 +8,15 @@ class EnumerableTest extends TestCase
 {
     public function test_at(): void
     {
-        $this->assertEquals(1, $this->vec([1, 2])->at(0));
-        $this->assertEquals(2, $this->vec([1, 2])->at(1));
-        $this->assertEquals(2, $this->vec([1, 2])->at(-1));
-        $this->assertEquals(1, $this->vec([1, 2])->at(-2));
+        $this->assertSame(1, $this->vec([1, 2])->at(0));
+        $this->assertSame(2, $this->vec([1, 2])->at(1));
+        $this->assertSame(2, $this->vec([1, 2])->at(-1));
+        $this->assertSame(1, $this->vec([1, 2])->at(-2));
 
-        $this->assertEquals(1, $this->map(['a' => 1, 'b' => 2])->at(0));
-        $this->assertEquals(2, $this->map(['a' => 1, 'b' => 2])->at(1));
-        $this->assertEquals(2, $this->map(['a' => 1, 'b' => 2])->at(-1));
-        $this->assertEquals(1, $this->map(['a' => 1, 'b' => 2])->at(-2));
+        $this->assertSame(1, $this->map(['a' => 1, 'b' => 2])->at(0));
+        $this->assertSame(2, $this->map(['a' => 1, 'b' => 2])->at(1));
+        $this->assertSame(2, $this->map(['a' => 1, 'b' => 2])->at(-1));
+        $this->assertSame(1, $this->map(['a' => 1, 'b' => 2])->at(-2));
     }
 
     public function test_at_out_of_bounds_positive(): void
@@ -29,5 +29,22 @@ class EnumerableTest extends TestCase
     {
         $this->expectException(IndexOutOfBoundsException::class);
         $this->vec([1, 2])->at(-3);
+    }
+
+    public function test_atOr(): void
+    {
+        $this->assertSame(1, $this->vec([1, 2])->atOr(0, 0));
+        $this->assertSame(2, $this->vec([1, 2])->atOr(1, -1));
+        $this->assertSame(2, $this->vec([1, 2])->atOr(-1, -1));
+        $this->assertSame(1, $this->vec([1, 2])->atOr(0, -2));
+        $this->assertSame('fb', $this->vec([1, 2])->atOr(2, 'fb'), 'out of bounds');
+        $this->assertSame('fb', $this->vec([1, 2])->atOr(-3, 'fb'), 'out of bounds');
+
+        $this->assertSame(1, $this->map(['a' => 1, 'b' => 2])->atOr(0, 0));
+        $this->assertSame(2, $this->map(['a' => 1, 'b' => 2])->atOr(1, -1));
+        $this->assertSame(2, $this->map(['a' => 1, 'b' => 2])->atOr(-1, -1));
+        $this->assertSame(1, $this->map(['a' => 1, 'b' => 2])->atOr(0, -2));
+        $this->assertSame('fb', $this->map(['a' => 1, 'b' => 2])->atOr(2, 'fb'), 'out of bounds positive');
+        $this->assertSame('fb', $this->map(['a' => 1, 'b' => 2])->atOr(-3, 'fb'), 'out of bounds negative');
     }
 }
